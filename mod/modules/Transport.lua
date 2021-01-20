8c1974b8241e7302b1ade5da7717c34dec8b4776
pv <p@siberx.dev>
2021-01-20 10:01:37 +0300

    Added vehicles export

--- mod/modules/Transport.lua
+++ mod/modules/Transport.lua
@@ -1,11 +1,12 @@
 local mod = ...
 local str = mod.require('mod/utils/str')
+local TweakDb = mod.require('mod/helpers/TweakDb')
 
 local TransportModule = {}
 TransportModule.__index = TransportModule
 
 function TransportModule:new()
-	local this = {}
+	local this = { tweakDb = TweakDb:new() }
 
 	setmetatable(this, TransportModule)
 
@@ -20,27 +21,56 @@ function TransportModule:release()
 	self.vehicleSystem = nil
 end
 
+function TransportModule:applySpec(specData)
+	if specData.Vehicles then
+		self:setVehicles(specData.Vehicles)
+	end
+end
+
+function TransportModule:fillSpec(specData)
+	local vehicleSpecs = self:getVehicles()
+
+	if vehicleSpecs then
+		specData.Vehicles = vehicleSpecs
+	end
+end
+
 function TransportModule:getVehicles()
-	return nil
+	if mod.env.is183() then
+		return nil
+	end
+
+	self.tweakDb:load('mod/data/tweakdb-meta')
+
+	local vehicleSpecs = {}
+	local vehicles = self.vehicleSystem:GetPlayerUnlockedVehicles()
+
+	for _, vehicle in ipairs(vehicles) do
+		local vehicleMeta = self.tweakDb:resolve(vehicle.recordID)
+
+		local vehicleSpec = {}
+
+		vehicleSpec[1] = str.without(vehicleMeta.type, 'Vehicle.')
+		vehicleSpec._comment = self.tweakDb:describe(vehicleMeta)
+		vehicleSpec._order = self.tweakDb:order(vehicleMeta)
+
+		table.insert(vehicleSpecs, vehicleSpec)
+	end
+
+	self.tweakDb:unload()
+
+	if #vehicleSpecs == 0 then
+		return nil
+	end
+
+	self.tweakDb:sort(vehicleSpecs)
+
+	return vehicleSpecs
 end
 
 function TransportModule:setVehicles(vehicles)
 	for _, vehicle in ipairs(vehicles) do
 		self.vehicleSystem:EnablePlayerVehicle(str.with(vehicle, 'Vehicle.', true, false))
-	end
-end
-
-function TransportModule:applySpec(specData)
-	if specData.Transport then
-		self:setVehicles(specData.Transport)
-	end
-end
-
-function TransportModule:fillSpec(specData)
-	local transportData = self:getVehicles()
-
-	if transportData then
-		specData.Transport = transportData
 	end
 end
 
