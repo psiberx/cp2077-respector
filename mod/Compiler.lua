f74fcf1e693ff1f00e423d5a36a422da7566478b
pv <p@siberx.dev>
2021-02-05 13:44:11 +0300

    Updated samples

--- mod/Compiler.lua
+++ mod/Compiler.lua
@@ -205,7 +205,7 @@ function Compiler:writeDefaultSpec()
 	specStore:writeSpec(nil, {
 		_comment = {
 			'This is just a placeholder.',
-			'If you want to try to load a spec then copy "samples/V.lua" here.',
+			'If you want to try to load a spec then copy "samples/Noname.lua" here.',
 		}
 	})
 end
--- specs/V.lua
+++ specs/V.lua
@@ -1,4 +1,4 @@
 -- This is just a placeholder.
--- If you want to try to load a spec then copy "samples/V.lua" here.
+-- If you want to try to load a spec then copy "samples/Noname.lua" here.
 return {
 }
